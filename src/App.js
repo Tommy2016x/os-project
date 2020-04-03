20b34d1590d84265de312103ef1cc963e4f67199
Tommy Carrascal <Carrascalx@gmail.com>
2020-04-02 23:02:51 -0400

    graph works

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import Main from './pages/main';
+import io from 'socket.io-client';
+
+const SERVER_URL = 'http://localhost:5000';
+const socket = io.connect(SERVER_URL);
 
 function App() {
   return (
     <div>
-      <Main />
+      <Main socket = { socket }/>
     </div>
   );
 }
--- src/pages/main/index.js
+++ src/pages/main/index.js
@@ -1,28 +1,89 @@
 import React, { Component } from 'react';
-import Graph from '../../components/graph';
 import './style.scss';
-import io from 'socket.io-client';
 
-const SERVER_URL = 'http://localhost:5000';
-const socket = io.connect(SERVER_URL);
+import GraphDisplay from "react-graph-vis";
+
+const options = {
+  layout: {
+    hierarchical: true
+  },
+  edges: {
+    color: "#000000"
+  },
+  height: "500px"
+};
+
+const events = {
+  select: function(event) {
+    var { nodes, edges } = event;
+  }
+};
 
 class Main extends Component{
   constructor(props){
     super(props);
 
-    socket.on('host-send_data', (data) => {
-      console.log(data);
-    })
+    this.state = {
+      graph: null
+    }
   }
 
   async componentDidMount(){
+    const { socket } = this.props;
+
+    socket.on('host-send_data', (data) => {
+      console.log('setting data');
+      this.setState({ graph: null })
+      let id = 1;
+      let ids = {};
+      const edges = [];
+      const nodes = [];
+
+      data.split('\n').forEach(line => {
+        const [from, to, count] = line.split(',');
+
+        if(!ids.hasOwnProperty(from)){
+          ids[from] = id++;
+          const newNode = { id: ids[from], label: from, title: from };
+          nodes.push(newNode)
+        }
+
+        if(!ids.hasOwnProperty(to)){
+          ids[to] = id++;
+          const newNode = { id: ids[to], label: to, title: to }
+          nodes.push(newNode);
+        }
+
+        const newEdge = { to: ids[to], from: ids[from], label: count }
+        edges.push(newEdge);
+      });
+      const graph = { nodes, edges }
+      console.log(graph)
+
+      this.setState({ graph });
+    });
+
     socket.emit('client-get_data');
   }
 
   render(){
+    const { graph } = this.state;
+
     return(
+      graph ? 
       <div className='container'>
-        <Graph />
+        <GraphDisplay
+          graph={ graph }
+          options={options}
+          events={events}
+          getNetwork={network => {
+            //  if you want access to vis.js network api you can set the state in a parent component using this property
+          }}
+        />
+      </div>
+      :
+      <div>
+        <h1>Loading...</h1>
       </div>
     )
   }
